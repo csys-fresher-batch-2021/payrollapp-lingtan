c7bef958c1b31a7014bf25c54ae35becba5e4c32
Lingtan1112 <lingtan1222k@gmail.com>
2021-05-11 23:09:26 +0530

    Rectified bugs 4

--- src/test/java/in/lingtan/validator/ValidatorTestCase.java
+++ src/test/java/in/lingtan/validator/ValidatorTestCase.java
@@ -14,18 +14,20 @@ public class ValidatorTestCase {
 	}
 	
 	@Test
+	public void isEmployeeIdValidCase() { //valid string is passed
+		String employeeName = "Ling";
+		boolean isValidEmployeeId = UserValidator.isEmployeeIdEmptyAndNull(employeeName);
+		assertTrue(isValidEmployeeId);
+	}
+	
+	
+	@Test
 	public void isEmployeeIdEmpty() { //employeeId is empty
 		String employeeName = "";
 		boolean isValidEmployeeId = UserValidator.isEmployeeIdEmptyAndNull(employeeName);
 		assertFalse(isValidEmployeeId);
 	}
 	
-	@Test
-	public void isEmployeeIdValidCase() { //valid string is passed
-		String employeeName = "Ling";
-		boolean isValidEmployeeId = UserValidator.isEmployeeIdEmptyAndNull(employeeName);
-		assertTrue(isValidEmployeeId);
-	}
 	
 //////////////////////////////////////////////////////////////////////////////////	
 	
@@ -39,37 +41,9 @@ public class ValidatorTestCase {
 	
 	@Test
 	public void isValidEmployeeIdInvalidLengthTest() { //Invalid employee Id is validated
-	String employeeName = "Ling267";
-	boolean isValidEmployeeId = UserValidator.isValidEmployeeIdLength(employeeName);
-	assertFalse(isValidEmployeeId);
-}
-
-	@Test
-	public void isValidFormatEmployyeeIdValidTest() { //Valid employeeId format is validated
-	String employeeName = "Lin2657";
-	boolean isValidEmployeeId = UserValidator.isValidEmployyeeIdFormat(employeeName);
-	assertFalse(isValidEmployeeId);
-	}
-	
-	@Test
-	public void isValidFormatEmployyeeIdInvalidNumberTest() { //Invalid employeeId with only 4 numerals is validated
-	String employeeName = "Ling257";
-	boolean isValidEmployeeId =UserValidator.isValidEmployyeeIdFormat(employeeName);
-	assertFalse(isValidEmployeeId);
-	}
-	
-	@Test
-	public void isValidFormatEmployyeeIdInvalidAlphabetTest() { // Invalid employeeId with only 3 alphabets is validated
-	String employeeName = "Lin2657";
-	boolean isValidEmployeeId =UserValidator.isValidEmployyeeIdFormat(employeeName);
-	assertFalse(isValidEmployeeId);
-	}
-	
-	@Test
-	public void isValidPasswordFormatInvalidTest() { //username is wrong password is correct
-		String password =  "raja123";
-		boolean isValidPasswordLength = UserValidator.isValidPasswordFormat(password);
-		assertFalse(isValidPasswordLength);
+		String employeeName = "Ling267";
+		boolean isValidEmployeeId = UserValidator.isValidEmployeeIdLength(employeeName);
+		assertFalse(isValidEmployeeId);
 	}
 	
 	@Test
@@ -79,6 +53,22 @@ public class ValidatorTestCase {
 		assertTrue(isValidPasswordLength);
 	}
 	
+
+	@Test
+		public void isValidFormatEmployyeeIdValidTest() { //Valid employeeId format is validated
+		String employeeName = "Lin2657";
+			boolean isValidEmployeeId = UserValidator.isValidEmployyeeIdFormat(employeeName);
+	assertFalse(isValidEmployeeId);
+	}
+	
+	
+	
+	@Test
+	public void isValidPasswordFormatInvalidTest() { //username is wrong password is correct
+		String password =  "raja123";
+		boolean isValidPasswordLength = UserValidator.isValidPasswordFormat(password);
+		assertFalse(isValidPasswordLength);
+	}
 	
 
 }
